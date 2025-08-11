d0324e11ab54898c9f254f693b293638ec3464f7
ConsultAdd <consultadd@ConsultAdds-MacBook-Pro.local>
2025-08-11 19:50:10 +0530

    WIP: work on Profile.jsx

--- frontend/src/component/Profile.jsx
+++ frontend/src/component/Profile.jsx
@@ -4,14 +4,9 @@ import React, { useState } from 'react'
 
 const Profile = () => {
   return (
-   <Container sx={{display:'flex' ,justifyContent:'center',alignItems:'center',height:'100vh',bgcolor:"black"}}>
-   <Box sx={{bgcolor:'green',height:'70vh',width:"50vw"}}>
-    <Box sx={{display:'flex'}}> 
-         <Typography>Name</Typography>
-         <input type="text" />
-    </Box>
-   </Box>
-   </Container>
+   <div style={{display:'flex' ,width:'100',justifyContent:'center',backgroundColor:'',alignItems:'center',border:'5px solid black',padding:'10px'}}>
+   
+   </div>
   )
 }
 
