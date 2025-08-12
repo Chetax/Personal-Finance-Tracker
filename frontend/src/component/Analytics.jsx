e0854b13536ee23780f4ee81dc96ca4b635c1cf4
ConsultAdd <consultadd@ConsultAdds-MacBook-Pro.local>
2025-08-13 02:02:34 +0530

    Feat:Done With Dashbaord

---
+++ frontend/src/component/Analytics.jsx
@@ -0,0 +1,14 @@
+import { Container ,Box} from '@mui/material'
+import React from 'react'
+
+const Analytics = () => {
+  return (
+    <Container>
+        <Box>
+   
+        </Box>
+    </Container>
+  )
+}
+
+export default Analytics
--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -1,44 +1,49 @@
 import { Box, Button, Container, Typography, TextField } from '@mui/material'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useCookies } from 'react-cookie';
+import { jwtDecode } from 'jwt-decode'
+import axios from 'axios';
+import { useUser } from '../context/UserContext';
 import toast from 'react-hot-toast';
 const Login = () => {
+  const {userId, setUserId } = useUser();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+ const [cookies, setCookie, removeCookie] = useCookies(['cookieName']);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+ const handleSubmit = async (e) => {
+  e.preventDefault();
   if (!email || !password) {
     alert("Please fill in all fields");
     return;
   }
-
   try {
-    const response = await fetch('http://127.0.0.1:8000/api/auth/login/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password })
+    const response = await axios.post('http://127.0.0.1:8000/api/auth/login/', {
+      email,
+      password
     });
+    const token = response.data;
 
-    const data = await response.json();
-
-    if (response.ok) {
-      toast.success('Login successful!');
-      console.log('Login response:', data);
-
-      navigate('/dashboard');
-    } else {
-      toast.error(data.detail || 'Login failed');
-    }
+    toast.success('Login successful!');
+    setCookie('cookie', token.access, { path: '/' });
+    const decoded = jwtDecode(token.access)
+   console.log("Decoded token:", decoded);
+    setUserId(decoded.user_id);
+    console.log("Set userId:", decoded.user_id);
+      console.log("Set userId:", userId);
+    
+    navigate('/dashboard');
   } catch (error) {
+    if (error.response && error.response.data) {
+      toast.error(error.response.data.detail || 'Login failed');
+    } else {
+      toast.error('Something went wrong!');
+    }
     console.error('Login error:', error);
-    toast.error('Something went wrong!');
   }
-  };
+};
 
   return (
        <div style={{backgroundColor:"#051221",overflow:'hidden'}}>
