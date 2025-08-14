224c59d57e48b5345563b5634dd1fd6ea1a00b96
ConsultAdd <consultadd@ConsultAdds-MacBook-Pro.local>
2025-08-14 17:27:06 +0530

    Doen With Dashboard and Analytics

--- backend/core/User/views.py
+++ backend/core/User/views.py
@@ -6,9 +6,3 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = UserSerializer
-
-    def get_queryset(self):
-        return User.objects.filter(id=self.request.user.id)
-
-    def perform_update(self, serializer):
-        serializer.save(id=self.request.user.id)
